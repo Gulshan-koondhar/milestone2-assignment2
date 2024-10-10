74bd542e968ee0f773699054b919790bae711652
Gulshan-koondhar <153608541+Gulshan-koondhar@users.noreply.github.com>
2024-10-10 13:49:08 +0500

    Portfolio Website using Tailwind CSS

---
+++ components/About.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Skills from "./Skills";
+import Image from "next/image";
+
+const About = () => {
+  return (
+    <div>
+      <div className="max-w-screen-xl m-auto p-4">
+        <div className="flex items-center justify-between mt-14 gap-20">
+          <div className="flex-1 flex-col gap-[1.2rem]">
+            <h2 className="text-[2.5rem] bg-gradient-to-tl from-[#a5d0d3] to-[#21a392] text-transparent bg-clip-text">
+              About
+            </h2>
+            <p className="text-justify text-[1.2rem]">
+              I’m Gulshan Mustafa, a passionate front-end web developer
+              enthusiastic about creating user-friendly and visually appealing
+              web experiences. I’ve embarked on a journey to transform my love
+              for coding into impactful digital solutions. During my academic
+              journey and beyond, I’ve honed my skills in HTML, CSS, and
+              JavaScript, with a keen interest in frameworks like React.js. My
+              projects, such as a responsive calculator, a dynamic weather app,
+              and an intuitive to-do list application, reflect my commitment to
+              learning and applying the latest web technologies. I thrive on
+              challenges that push me to innovate and solve problems creatively.
+              Each project I undertake not only strengthens my technical
+              abilities but also reinforces my dedication to delivering clean,
+              efficient code that enhances user experience. Currently, I’m eager
+              to contribute my skills and learn from experienced professionals
+              in the field. I’m excited about opportunities to collaborate on
+              meaningful projects where I can contribute my enthusiasm for
+              front-end development and continue to grow as a developer.
+            </p>
+            <div>
+              <h2 className="text-[2.5rem] bg-gradient-to-tl from-[#a5d0d3] to-[#21a392] text-transparent bg-clip-text">
+                Skills
+              </h2>
+              <div className="flex items-center justify-between mt-[10px]">
+                <Skills src="/html.png" />
+                <Skills src="/css.png" />
+                <Skills src="/js.png" />
+                <Skills src="/Typescript.png" />
+                <Skills src="/tailwind.png" />
+                <Skills src="/React.png" />
+                <Skills src="/nextjs.png" />
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center items-center">
+            <Image
+              src="/about.png"
+              alt="Profile Picture"
+              width={400}
+              height={400}
+              className="rounded-full"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ components/Hero.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Facebook, Github, Instagram, Linkedin } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+const Hero = () => {
+  return (
+    <div>
+      <div className="max-w-screen-xl m-auto p-4">
+        <div className="flex items-center justify-between w-full h-full mt-[60px] gap-20">
+          <div className="flex-1 flex-col gap-[1.2rem]">
+            <h2 className="text-xl">
+              Hi, I&apos;m{" "}
+              <span className="text-3xl bg-gradient-to-tl from-[#a5d0d3] to-[#21a392] text-transparent bg-clip-text">
+                {" "}
+                Gulshan Mustafa
+              </span>
+            </h2>
+            <p className="text-[1.2rem] mt-[10px]">
+              A passionate front-end web developer enthusiastic about creating
+              user-friendly and visually appealing web experiences.
+            </p>
+
+            <div>
+              <ul className="flex items-center gap-8 mt-[10px]">
+                <li>
+                  <Link href="">
+                    <Facebook className="text-white hover:text-black" />
+                  </Link>
+                </li>
+                <li>
+                  <Link href="">
+                    <Linkedin className="text-white hover:text-black" />
+                  </Link>
+                </li>
+                <li>
+                  <Link href="">
+                    <Github className="text-white hover:text-black" />
+                  </Link>
+                </li>
+                <li>
+                  <Link href="">
+                    <Instagram className="text-white hover:text-black" />
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="flex gap-8">
+              <button className="p-[10px] outline-none ring-1 ring-gray-200 hover:bg-gray-600 hover:text-black hover:ring-black mt-[15px] mr-[10px] rounded-lg text-lg">
+                <Link href="/My Resume.pdf">Download CV</Link>
+              </button>
+              <button className="p-[10px] outline-none ring-1 ring-gray-200 hover:bg-gray-600 hover:text-black hover:ring-black  mt-[15px] mr-[10px] rounded-lg text-lg">
+                <Link href="/about">About Me</Link>
+              </button>
+            </div>
+          </div>
+          <div className="flex-1 items-center justify-center">
+            <Image
+              src="/hero.png"
+              alt="Profile Picture"
+              className="rounded-full"
+              width={400}
+              height={400}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
